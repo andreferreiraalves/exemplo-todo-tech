1c9cf7d6fb5f9a3ad0d4609f2976efe88811dddf
André Ferreira <andre.ferreira.alves@gmail.com>
2024-08-01 08:37:52 -0300

    exemplo fetch data

--- lol.js
+++ lol.js
@@ -1,25 +1,49 @@
 const btn = document.getElementById('botao');
 const imagens = document.getElementById('lista');
 
-btn.addEventListener('click', () => {
-    fetch('https://rickandmortyapi.com/api/character/1,2,3')
-        .then(response => response.json())
-        .then(dados => {
-            console.log(dados)
-            dados.forEach(dado => {
+btn.addEventListener('click', async () => {
+    const response = await fetch('https://rickandmortyapi.com/api/character/1,2,3');
+    const dados = await response.json();
 
-                const item = document.createElement('div');
+    console.log(dados);
 
-                const imagem = document.createElement('img');
-                imagem.src = dado.image;
 
-                const nome = document.createElement('p');
-                nome.innerText = dado.name
+    dados.forEach(dado => {
+        const item = document.createElement('div');
 
-                item.appendChild(imagem)
-                item.appendChild(nome);
+        const imagem = document.createElement('img');
+        imagem.src = dado.image;
 
-                imagens.appendChild(item);
-            });
-        })
+        const nome = document.createElement('p');
+        nome.innerText = dado.name
+
+        item.appendChild(imagem)
+        item.appendChild(nome);
+
+        imagens.appendChild(item);
+    });
 })
+
+
+// btn.addEventListener('click', () => {
+//     fetch('https://rickandmortyapi.com/api/character/1,2,3')
+//         .then(response => response.json())
+//         .then(dados => {
+//             console.log(dados)
+//             dados.forEach(dado => {
+
+//                 const item = document.createElement('div');
+
+//                 const imagem = document.createElement('img');
+//                 imagem.src = dado.image;
+
+//                 const nome = document.createElement('p');
+//                 nome.innerText = dado.name
+
+//                 item.appendChild(imagem)
+//                 item.appendChild(nome);
+
+//                 imagens.appendChild(item);
+//             });
+//         })
+// })
